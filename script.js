5cc7f08e8cf20941522a7d025075e20e42d21a96
vanegasc811-rgb <vanegasc811@gmail.com>
2026-06-29 13:26:58 +0200

    bloque uno script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+/* ==========================================
+   PROYECTO LUNA
+   JavaScript principal
+========================================== */
 
+// Elementos
+const envelope = document.querySelector(".envelope");
+const flap = document.querySelector(".envelope-flap");
+const seal = document.querySelector(".seal");
+
+// Estados
+let opened = false;
+
+// Eventos
+envelope.addEventListener("click", openEnvelope);
+
+// Función principal
+function openEnvelope() {
+
+    if (opened) return;
+
+    opened = true;
+
+    console.log("Proyecto Luna comenzó ❤️");
+
+}
